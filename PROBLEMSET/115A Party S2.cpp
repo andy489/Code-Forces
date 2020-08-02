b130a3b8fbacebbf66a95b376a8aeda8c9181ab4
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-02 19:11:39 +0300

    Create 115A Party S2.cpp

---
+++ PROBLEMSET/115A Party S2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, i(1), a[2001], dep, curr, min(0);
+    for (cin >> n; i <= n;)
+        cin >> a[i++];
+    for (i = 1; i <= n;) {
+        dep = 0;
+        curr = a[i++];
+        while (curr != -1)
+            ++dep, curr = a[curr];
+        min = (dep > min ? dep : min);
+    }
+    cout << ++min;
+}
