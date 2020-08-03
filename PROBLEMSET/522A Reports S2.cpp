d96200ac9d8992883b1981b7e2cca8e6f250d915
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-03 21:01:41 +0300

    Create 522A Reports S2.cpp

---
+++ PROBLEMSET/522A Reports S2.cpp
@@ -0,0 +1,31 @@
+// github.com/andy489
+
+#include <iostream>
+#include <map>
+
+#define q string
+
+using namespace std;
+
+int n, k = 0;
+q s, z;
+map<q, int> m;
+
+q l(q s) {
+    for (int j = 0; j < s.size(); ++j)
+        s[j] = tolower(s[j]);
+    return s;
+}
+
+int main() {
+    cin >> n;
+    while (n--) {
+        cin >> s >> z >> z;
+        s = l(s);
+        z = l(z);
+        m[s] = m[z] + 1;
+        k = max(k, m[z] + 1);
+    }
+    cout << k + 1;
+    return 0;
+}
