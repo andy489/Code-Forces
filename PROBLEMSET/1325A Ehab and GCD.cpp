8646d13b6cae25208338631ba8d8995759c90738
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-23 01:18:09 +0300

    Create 1325A Ehab and GCD.cpp

---
+++ PROBLEMSET/1325A Ehab and GCD.cpp
@@ -0,0 +1,16 @@
+// github.com/andy489
+
+// https://codeforces.com/problemset/problem/1325/A
+
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+    int t, x;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d", &x);
+        printf("1 %d\n", x - 1);
+    } return 0;
+}
