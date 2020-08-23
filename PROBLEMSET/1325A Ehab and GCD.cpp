629529745ab40779f8f52d99dd33e59ad2e30a97
Andrey Stoev <52545634+andy489@users.noreply.github.com>
2020-08-23 09:38:41 +0300

    Update 1325A Ehab and GCD.cpp

--- PROBLEMSET/1325A Ehab and GCD.cpp
+++ PROBLEMSET/1325A Ehab and GCD.cpp
@@ -4,13 +4,11 @@
 
 #include <stdio.h>
 
-using namespace std;
-
 int main() {
     int t, x;
     scanf("%d", &t);
     while (t--) {
         scanf("%d", &x);
         printf("1 %d\n", x - 1);
-    } return 0;
+    }
 }
